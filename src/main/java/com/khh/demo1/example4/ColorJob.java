4f19b306c80a5e57a020e450cd0525b412198ce4
kuanghh <951087952@qq.com>
2017-07-20 16:06:18 +0800

    job的参数传递和状态保存

--- src/main/java/com/khh/demo1/example4/ColorJob.java
+++ src/main/java/com/khh/demo1/example4/ColorJob.java
@@ -10,12 +10,12 @@ import java.util.Date;
  * 如果你想在 某个job执行的时候传入参数,参数在job执行过程中对参数有所修改,并且在job执行完毕后把参数返回
  *  那么你需要学习一下现在的这个例子了,因为它正是你所想要的
  */
-@PersistJobDataAfterExecution
-@DisallowConcurrentExecution
+@PersistJobDataAfterExecution//保存在JobDataMap传递的参数
+@DisallowConcurrentExecution//保证多个任务间不会同时执行.所以在多任务执行时最好加上
 public class ColorJob implements Job{
 
     public static final String FAVORITE_COLOR = "favorite_color";
-    public static final String EXECUTION_COLOR = "execution_color";
+    public static final String EXECUTION_COUNT = "count";
 
     /**
      * 由于Quartz每次执行时都将重新实例化一个类，成员不能使用非静态成员变量来维护状态！
@@ -32,7 +32,7 @@ public class ColorJob implements Job{
         JobDataMap data = jobExecutionContext.getJobDetail().getJobDataMap();
         String favoriteColor = data.getString(FAVORITE_COLOR);
 
-        int count = data.getInt(EXECUTION_COLOR);
+        int count = data.getInt(EXECUTION_COUNT);
 
         System.out.println("ColorJob: " + jobKey + " executing at " + new Date() + "\n" +
                 "  favorite color is " + favoriteColor + "\n" +
@@ -42,7 +42,7 @@ public class ColorJob implements Job{
         //增加计数并将其存储回
         //作业映射，以便正确维护作业状态。
         count++;
-        data.put(EXECUTION_COLOR,count);
+        data.put(EXECUTION_COUNT,count);
 
         //增加本地成员变量
         //由于工作状态不能通过成员变量来维护，所以这没有真正的用途。
--- src/main/java/com/khh/demo1/example4/JobStateExample.java
+++ src/main/java/com/khh/demo1/example4/JobStateExample.java
@@ -1,8 +1,7 @@
 package com.khh.demo1.example4;
 
-import org.quartz.DateBuilder;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerFactory;
+import com.khh.demo1.example3.SimpleJob;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.util.Date;
@@ -21,5 +20,56 @@ public class JobStateExample {
         Date startTime = DateBuilder.nextGivenSecondDate(null, 10);
 
 
+        //job1 将会 跑5次（运行1次，重复4次）,每隔10秒 一次
+        JobDetail job1 = JobBuilder.newJob(ColorJob.class).withIdentity("job1", "group1").build();
+
+        SimpleTrigger trigger1 = TriggerBuilder.newTrigger().withIdentity("trigger1","group1")
+                .startAt(startTime).withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).withRepeatCount(4)).build();
+
+        //将初始化参数传递到作业中
+        job1.getJobDataMap().put(ColorJob.FAVORITE_COLOR,"Green");
+        job1.getJobDataMap().put(ColorJob.EXECUTION_COUNT,1);
+
+        //调度工作去跑
+        Date scheduleTime1 = scheduler.scheduleJob(job1,trigger1);
+        System.out.println(job1.getKey() + " will run at: " + scheduleTime1 + " and repeat: " + trigger1.getRepeatCount()
+                + " times, every " + trigger1.getRepeatInterval() / 1000 + " seconds");
+
+        System.out.println("----------------------------------------------------------------------------------------------------");
+
+        //job2 will also run 5 times, every 10 seconds
+
+        JobDetail job2 = JobBuilder.newJob(ColorJob.class).withIdentity("job2", "group1").build();
+
+        SimpleTrigger trigger2 = TriggerBuilder.newTrigger().withIdentity("trigger2","group1")
+                .startAt(startTime).withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).withRepeatCount(4)).build();
+
+        //将初始化参数传递到作业中
+        job2.getJobDataMap().put(ColorJob.FAVORITE_COLOR,"Yellow");
+        job2.getJobDataMap().put(ColorJob.EXECUTION_COUNT,1);
+
+        Date scheduleTime2 = scheduler.scheduleJob(job2,trigger2);
+        System.out.println(job1.getKey() + " will run at: " + scheduleTime2 + " and repeat: " + trigger2.getRepeatCount()
+                + " times, every " + trigger2.getRepeatInterval() / 1000 + " seconds");
+
+        System.out.println("----------------调度开始--------------------------");
+        scheduler.start();
+        System.out.println("----------------等60秒----------------------------");
+        try {
+            // wait five minutes to show jobs
+            Thread.sleep(60L * 1000L);
+            // executing...
+        } catch (Exception e) {
+            //
+        }
+        scheduler.shutdown(true);
+        System.out.println("----------------调度结束--------------------------");
+
+        SchedulerMetaData metaData = scheduler.getMetaData();
+        System.out.println("Executed " + metaData.getNumberOfJobsExecuted() + " jobs.");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new JobStateExample().run();
     }
 }
