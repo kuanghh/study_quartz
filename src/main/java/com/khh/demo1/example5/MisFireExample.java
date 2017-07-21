3057e44a47a0867c73fa38336439693edb4b98e8
kuanghh <951087952@qq.com>
2017-07-21 17:36:31 +0800

    触发丢失,解决方法

---
+++ src/main/java/com/khh/demo1/example5/MisFireExample.java
@@ -0,0 +1,64 @@
+package com.khh.demo1.example5;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.Date;
+
+/**
+ * Created by FSTMP on 2017/7/21.
+ *  misfire -- 指的是 错过了触发时间
+ */
+public class MisFireExample {
+
+    public void run() throws Exception {
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+
+        Date startTime = DateBuilder.nextGivenSecondDate(null, 15);
+
+        //job1 触发器设定每3秒钟触发一次，但是工作需要10秒钟的执行时间(延迟了10秒)
+        JobDetail job = JobBuilder.newJob(StatefulDumbJob.class).withIdentity("statefulJob1", "group1")
+                .usingJobData(StatefulDumbJob.EXECUTIONS_DELAY, 10000L).build();
+
+        SimpleTrigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startAt(startTime)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(3).repeatForever()).build();
+
+        Date ft = scheduler.scheduleJob(job, trigger);
+        System.out.println(job.getKey() + " will run at: " + ft + " and repeat: " + trigger.getRepeatCount() + " times, every "
+                + trigger.getRepeatInterval() / 1000 + " seconds");
+
+        //job2 触发器设定每3秒钟触发一次，但是工作需要10秒钟的执行时间
+        //当检测到丢失触发时，不会立即触发，而是忽略本次安排到下一个预定时间去触发
+        job = JobBuilder.newJob(StatefulDumbJob.class).withIdentity("statefulJob2", "group1")
+                .usingJobData(StatefulDumbJob.EXECUTIONS_DELAY, 10000L).build();
+
+        trigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group1").startAt(startTime)
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(3)
+                        .repeatForever()
+                        .withMisfireHandlingInstructionNowWithExistingCount()).build();
+
+        ft = scheduler.scheduleJob(job, trigger);
+        System.out.println(job.getKey() + " will run at: " + ft + " and repeat: " + trigger.getRepeatCount() + " times, every "
+                + trigger.getRepeatInterval() / 1000 + " seconds");
+
+        scheduler.start();
+        System.out.println("-----------------------------------调度开始----------------------------");
+
+        try {
+            Thread.sleep(600L * 1000L);
+        } catch (Exception e) {
+
+        }
+        scheduler.shutdown(true);
+        System.out.println("-----------------------------------调度结束----------------------------");
+
+        SchedulerMetaData metaData = scheduler.getMetaData();
+        System.out.println("Executed " + metaData.getNumberOfJobsExecuted() + " jobs.");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new MisFireExample().run();
+    }
+}
--- src/main/java/com/khh/demo1/example5/StatefulDumbJob.java
+++ src/main/java/com/khh/demo1/example5/StatefulDumbJob.java
@@ -2,19 +2,54 @@ package com.khh.demo1.example5;
 
 import org.quartz.*;
 
+import java.util.Date;
+
 /**
  * Created by FSTMP on 2017/7/20.
+ *
+ * 这个工作为了单元测试的目的
  */
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 public class StatefulDumbJob implements Job {
 
+    // 静态常量，作为任务在调用间，保持数据的键(key)
+    // NUM_EXECUTIONS，保存的计数每次递增1
+    // EXECUTION_DELAY，任务在执行时，中间睡眠的时间。本例中睡眠时间过长导致了错失触发
     public static final String NUM_EXECUTIONS = "NumExecutions";
-
     public static final String EXECUTIONS_DELAY = "ExecutionDelay";
 
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public StatefulDumbJob(){
 
     }
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+        System.out.println("---" + context.getJobDetail().getKey()  + " executing.[" + new Date() + "]");
+
+        JobDataMap map = context.getJobDetail().getJobDataMap();
+
+        int executeCount = 0;
+        if(map.containsKey(NUM_EXECUTIONS)){
+            executeCount = map.getInt(NUM_EXECUTIONS);
+        }
+
+        executeCount++;
+
+        map.put(NUM_EXECUTIONS,executeCount);
+        //睡眠时间: 由调度类重新设置值 ,本例为 睡眠10s
+        long deley = 50001;
+        if(map.containsKey(EXECUTIONS_DELAY)){
+            deley = map.getLong(EXECUTIONS_DELAY);
+        }
+        try{
+            Thread.sleep(deley);
+        }catch (Exception e){
+
+        }
+        System.out.println("  -" + context.getJobDetail().getKey() + " complete (" + executeCount + ").");
+    }
+
+
 }
